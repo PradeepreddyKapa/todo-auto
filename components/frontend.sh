f3e8d9dadbf5138251eb66ff20c1b44f654077ed
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-03 17:51:24 +0530

    ss

--- components/frontend.sh
+++ components/frontend.sh
@@ -8,8 +8,6 @@ Head "Installing Nginx and Npm"
 sudo apt install nginx -y &>>$LOG
 sudo apt install npm -y &>>$LOG
 sudo apt install nodejs -y &>>$LOG
-sudo apt install node-node-sass &>>$LOG
-
 stat $?
 
 Head "Downloading COMPONENT"
