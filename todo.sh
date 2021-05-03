277df6370ca0dff2cf6c1eb2d4a13d755db6be37
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-03 16:39:52 +0530

    ss

--- todo.sh
+++ todo.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+COMPONENT=$1
+
+
+export LOG=/tmp/${COMPONENT}.log
+rm -f $LOG
+
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root" ]; then
+  ERROR "You should be a root user to perform this script, Try with sudo"
+  exit 1
+fi
