ceda49458a585267f23b8352df116990b9a2371f
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-04 12:14:05 +0530

    ss

--- components/login.sh
+++ components/login.sh
@@ -2,3 +2,31 @@
 
 source components/common.sh
 OS_PREREQ
+
+Head "Installing Go"
+apt install golang -y &>>$LOG
+Stat $?
+
+Head "Cloning Repo"
+git clone https://github.com/PradeepreddyKapa/login.git &>>$LOG && cd login
+Stat $?
+
+
+Head "Creating Service"
+
+mv systemd.service /etc/systemd/system/login.service
+
+Head "Get dependencies"
+
+go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
+Stat $?
+
+Head "Building Package"
+
+go build &>>$LOG
+Stat $?
+
+Head "Restarting Services"
+
+systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
+Stat $?
