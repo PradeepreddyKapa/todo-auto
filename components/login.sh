ae619cb38325eeffe1c1eeb1ad9345095fb8258b
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-04 13:03:18 +0530

    ss

--- components/login.sh
+++ components/login.sh
@@ -12,17 +12,14 @@ git clone https://github.com/PradeepreddyKapa/login.git &>>$LOG && cd login && m
 Stat $?
 
 Head "Get dependencies"
-
 go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
 Stat $?
 
 Head "Building Package"
-
 go build &>>$LOG
 Stat $?
 
 Head "Restarting Services"
-
 ./login
 #systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
 Stat $?
